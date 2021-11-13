833e1ae9d4327666a64c9684a4e7db6ce39de6c9
chungsam <tieoneease@gmail.com>
2021-11-12 19:51:28 -0500

    set up fabric

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { fabric } from 'fabric';
+	import { onMount } from 'svelte';
+
+	let canvas;
+
+	onMount(() => {
+		let canv = new fabric.Canvas(canvas);
+
+		const rect = new fabric.Rect({
+			left: 10,
+			top: 10,
+			width: 20,
+			height: 15,
+			fill: 'blue'
+		});
+
+		const circle = new fabric.Circle({
+			radius: 20,
+			fill: 'blue',
+			left: 100,
+			top: 100
+		});
+
+		canv.add(circle);
+
+		canv.add(rect);
+	});
+</script>
+
+<canvas bind:this={canvas} width="500" height="300" />
